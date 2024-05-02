1af652cb82ca9c193ea9f725479093146f0ac53b
Toshimasa Hashiguchi <61375543+t0ottio13@users.noreply.github.com>
2024-05-02 13:30:29 +0000

    シーンの初期設定

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import * as THREE from 'three';
+
+let renderer, scene, camera;
+
+function init(){
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+
+    renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setSize(window.innerHeight, window.innerHeight);
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera();
+
+    const light = new THREE.DirectionalLight();
+    scene.add(light);
+
+    render();
+}
+
+function render(){
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+
+init();
